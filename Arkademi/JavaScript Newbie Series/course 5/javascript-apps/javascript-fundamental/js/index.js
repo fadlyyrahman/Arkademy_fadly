ea2c719a3c63a0ffe4e2c30ce078d6085396dccc
fadlyyrahman <50941694+fadlyyrahman@users.noreply.github.com>
2019-05-23 13:40:00 +0700

    Add files via upload
    Arkademy Course

---
+++ Arkademi/JavaScript Newbie Series/course 5/javascript-apps/javascript-fundamental/js/index.js
@@ -0,0 +1,45 @@
+//String
+
+var myName = "Zaidan";
+var address = "caringin bogor";
+var my_name = "nde";
+var MY_NAME = "	nde";
+
+//Integer
+
+var firstNumber = 1;
+var secondNumber = 2;
+
+//Decimal
+
+var someDecimal = 2.8;
+
+//Boolean
+
+var isHandsome = true;
+
+//Array
+
+var snack = ['Pecel', 'Pecel Ayam', 'Pecel bebek'];
+
+//Object
+var car = {
+	name: "Datsun",
+	color: "black",
+	cc: 2000
+}
+
+//console.log(myName);
+//console.log(address);
+//console.log(my_name);
+//console.log(MY_NAME);
+//console.log(firstNumber);
+//console.log(secondNumber);
+//console.log(someDecimal);
+//console.log(isHandsome);
+//console.log(snack);
+console.log(car);
+console.log(car.name);
+console.log(car.color);
+console.log(car.cc);
+
---
+++ Arkademi/JavaScript Newbie Series/course 8/javascript-apps/javascript-fundamental/js/index.js
@@ -0,0 +1,10 @@
+	var person = {
+			firstName : "Ratu",
+			lastName : "Febrianty",
+			age : 17,
+			walk(){
+				return "I am walking"
+			}
+	}
+
+	console.log(person['firstName']);
